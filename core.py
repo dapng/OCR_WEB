9e4f35307d5efd5cbdb2e61a12f51d7f5e6f3f73
Данила Богославцев <da.png@yandex.ru>
2021-08-28 23:01:05 +0300

    fixed app

--- core.py
+++ core.py
@@ -1,11 +1,22 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+# try:
+#     from PIL import Image
+# except ImportError:
+#     import Image
+# import pytesseract
+
+
+from PIL import Image
 import pytesseract
 
 
+# def ocr_core(filename):
+#     photo = Image.open(filename)
+#     text_go = pytesseract.image_to_string(photo, lang='rus+eng')
+#     text = text_go
+#     return text
+# print(ocr_core('images/2.jpg'))
+
 def ocr_core(filename):
-    photo = Image.open(filename)
-    text = pytesseract.image_to_string(photo, lang='rus+eng')
+    text = pytesseract.image_to_string(Image.open(filename), lang='rus+eng')
     return text
+# print(ocr_core('images/1.jpg'))
