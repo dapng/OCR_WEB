45ccf006d75b64eb167de28116f5298201243753
Данила Богославцев <da.png@yandex.ru>
2021-08-28 17:56:27 +0300

    fixed path route

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_page():
     if request.method == 'POST':
         if 'file' not in request.files:
